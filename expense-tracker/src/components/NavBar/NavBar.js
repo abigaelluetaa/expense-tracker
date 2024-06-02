f8cd6e8b0c77da8eb92c9facd3ed861fb26f6f02
Abigael Amelia <abigaellueta29@gmail.com>
2024-06-02 11:20:12 -0700

    mini progress

--- expense-tracker/src/components/NavBar/NavBar.js
+++ expense-tracker/src/components/NavBar/NavBar.js
@@ -1,12 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from 'react';
 
 export default function Nav() {
-    return(
-        <div>
-            <Link href='/'> Homepage </Link>
-            <Link href='/dashboard'> Dashboard </Link>
-            <Link href='/reports'> Reports </Link>
-            <Link href='/login'> Login </Link>
+    // Temporary session state
+    const [session, setSession] = useState(false); //set true/false to test
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <Link href="/">
+            <span className="logo">Expense Tracker</span>
+          </Link>
+          <div className="nav-links">
+            <Link href="/dashboard"><span>Dashboard</span></Link>
+            <Link href="/reports"><span>Reports</span></Link>
+            {session ? (
+            <span className="cursor-pointer" onClick={() => setSession(false)}>Logout</span>
+            ) : (
+              <Link href="/login"><span>Login</span></Link>
+            )}
+          </div>
         </div>
-    )
-}
+      </nav>
+    );
+  }
+
